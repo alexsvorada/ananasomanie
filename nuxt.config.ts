b80c9fdc1d8a5855010a479dfe58141e4030ef8a
alexsvorada <alex@svorada.eu>
2024-12-15 23:38:51 +0100

    feat: initial commit <for project preview>

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/icon', '@vueuse/motion/nuxt', '@nuxt/fonts'],
+	tailwindcss: {
+		config: {
+			theme: {
+				extend: {
+					colors: {
+						primary: '#FFB23F',
+						dark: '#1A1A1A',
+						light: '#F5F5F5',
+						accent: '#FF6B6B',
+					},
+				},
+			},
+		},
+	},
+})
